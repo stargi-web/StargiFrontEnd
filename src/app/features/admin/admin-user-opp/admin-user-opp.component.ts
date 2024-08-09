d65d7e6f697a2606147cc24f5875ae0bcbacf77a
elbrocas12 <fanchesso156@hotmail.com>
2024-08-09 16:38:56 -0500

    feat: add admin modify or delete opps

--- src/app/features/admin/admin-user-opp/admin-user-opp.component.ts
+++ src/app/features/admin/admin-user-opp/admin-user-opp.component.ts
@@ -7,20 +7,32 @@ import { CommonModule } from '@angular/common';
 import { OpportunityRecordService } from '../../../services/opportunityRecordService';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { ExecutiveRecordsOppDialogComponent } from '../../executive/executive-records-opp-dialog/executive-records-opp-dialog.component';
+import { ConfirmDeleteOpportunityDialogComponent } from '../../../shared/components/confirm-delete-opportunity-dialog/confirm-delete-opportunity-dialog.component';
+import { CalendarModule } from 'primeng/calendar';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { FormsModule } from '@angular/forms';
+import { opportunityTypes, products, productTypes, states } from '../../../shared/const/constantes';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-admin-user-opp',
   providers:[DialogService],
   standalone: true,
-  imports: [TableModule,CommonModule],
+  imports: [InputTextModule,DropdownModule,FormsModule,CalendarModule,InputNumberModule,TableModule,CommonModule],
   templateUrl: './admin-user-opp.component.html',
   styleUrl: './admin-user-opp.component.css'
 })
 export class AdminUserOppComponent implements OnInit{
-
+  editingRowIndex: number | null = null;
+  urgentOpportunitiesCount: number = 0;
   sort=-1
   opportunities!:OpportunityModel[];
   loading=true;
   userId?:number;
+  opportunityTypes=opportunityTypes;
+  products=products;
+  productTypes=productTypes;
+  states=states;
   constructor(public dialogService:DialogService,private route:ActivatedRoute,private opportunityService:OpportunityService,private oppRecordService:OpportunityRecordService){}
   ref:DynamicDialogRef|undefined;
   ngOnInit(): void {
@@ -43,14 +55,97 @@ export class AdminUserOppComponent implements OnInit{
     })
   }
 
-  openRecordsDialog(oppId:number) {
+  calculateUrgentOpportunities() {
+    const today = new Date();
+    this.urgentOpportunitiesCount = this.opportunities.filter(opportunity => {
+      const estimatedClosingDate = new Date(opportunity.estimatedClosingDate);
+      const timeDiff = estimatedClosingDate.getTime() - today.getTime();
+      const dayDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+      return dayDiff <= 5;
+    }).length;
+  }
+  isUrgent(opportunity: OpportunityModel): boolean {
+    const today = new Date();
+    const estimatedClosingDate = new Date(opportunity.estimatedClosingDate);
+    const timeDiff = estimatedClosingDate.getTime() - today.getTime();
+    const dayDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    return dayDiff <= 5;
+  }
+  startEditing(rowIndex: number) {
+    this.editingRowIndex = rowIndex;
+  }
+
+  saveChanges(opportunity: OpportunityModel) {
+    const editCommand: any = {
+      oppId: opportunity.id!,
+      ruc: opportunity.ruc.toString(),
+      businessName: opportunity.businessName,
+      sfaNumber: opportunity.SfaNumber,
+      oppSfaDateCreation: opportunity.oppSfaDateCreation,
+      type: opportunity.type,
+      product: opportunity.product,
+      productType:opportunity.productType,
+      otherDetails: opportunity.otherDetails,
+      amount: opportunity.amount!,
+      newClosingDate: opportunity.estimatedClosingDate,
+      newUnits: opportunity.units,  // Puedes ajustar este valor según sea necesario
+      newState: opportunity.state,
+      newCommentary: opportunity.commentary,
+      contactName: opportunity.contactName || '',
+      contactNumber: opportunity.contactNumber || '',
+    };
+  
+    this.opportunityService.editOpportunity(editCommand).subscribe(
+      {
+        next: response => {
+          alert(`${response.message}`);
+          this.editingRowIndex = null;
+          this.loadOpportunities();  // Recargar la lista de oportunidades
+        },
+        error: error => {
+          console.error(error);
+          this.editingRowIndex = null;
+        }
+      }
+    );
+  }
+
+  cancelEditing() {
+    this.editingRowIndex = null;
+  }
+  deleteOpportunity(name: string,id:number) {
     const config={
       data:{
-        oppId
+        bussinesName:name
       },
-      Headers:'Historial de cambios',
-      with:'60vw',
+      Headers:'Confimar eliminacion'
     }
-    this.ref=this.dialogService.open(ExecutiveRecordsOppDialogComponent,config);
+    this.ref=this.dialogService.open(ConfirmDeleteOpportunityDialogComponent,config);
+    this.ref.onClose.subscribe((response:boolean)=>{
+      if(response){
+        this.opportunityService.deleteOpportunity(id).subscribe(
+          {
+            next:response=>{
+              this.opportunities=this.opportunities.filter(o=>o.id!==id);
+            },error:error=>{
+              console.error(error);
+            }
+          }
+        )
+      }
+    })
+  }
+  openRecordsDialog(oppId:number) {
+    if(this.editingRowIndex==null){
+
+      const config={
+        data:{
+          oppId
+        },
+        Headers:'Historial de cambios',
+        with:'60vw',
+      }
+      this.ref=this.dialogService.open(ExecutiveRecordsOppDialogComponent,config);
+    }
   }
 }
