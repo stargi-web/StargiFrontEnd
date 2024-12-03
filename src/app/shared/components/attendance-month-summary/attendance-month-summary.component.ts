50910a3341d744611f01ce24232d58203aae398e
elbrocas12 <fanchesso156@hotmail.com>
2024-12-03 09:41:30 -0500

    feat: fix monthly summaryuser attendance

--- src/app/shared/components/attendance-month-summary/attendance-month-summary.component.ts
+++ src/app/shared/components/attendance-month-summary/attendance-month-summary.component.ts
@@ -3,23 +3,31 @@ import { AttendanceService } from '../../../services/attendanceService';
 import { CommonModule } from '@angular/common';
 import { TooltipModule } from 'primeng/tooltip';
 
+interface Attendance {
+  date: string; 
+  state: string; 
+}
+
 @Component({
   selector: 'app-attendance-month-summary',
   standalone: true,
-  imports: [CommonModule,TooltipModule],
+  imports: [CommonModule, TooltipModule],
   templateUrl: './attendance-month-summary.component.html',
-  styleUrl: './attendance-month-summary.component.css'
+  styleUrls: ['./attendance-month-summary.component.css']
 })
 export class AttendanceMonthSummaryComponent implements OnInit {
-  weekDaysInMonth:{ date: Date }[] = [];
-  attendances: Date[] = [];
-  userId!:number;
-  constructor(private attendanceService:AttendanceService){}
+  weekDaysInMonth: { date: Date }[] = [];
+  attendances: Attendance[] = [];
+  userId!: number;
+
+  constructor(private attendanceService: AttendanceService) {}
+
   ngOnInit(): void {
-    this.userId=Number(sessionStorage.getItem("userId"))
+    this.userId = Number(sessionStorage.getItem("userId"));
     this.initializeWeekdaysInMonth();
     this.fetchAttendances();
   }
+
   initializeWeekdaysInMonth() {
     const now = new Date();
     const year = now.getFullYear();
@@ -41,12 +49,20 @@ export class AttendanceMonthSummaryComponent implements OnInit {
     const startDate = new Date(year, month, 1).toISOString();
     const endDate = new Date(year, month + 1, 0).toISOString();
 
-    this.attendanceService.getAttendancesByUserAndDates(this.userId,startDate,endDate)
+    this.attendanceService.getAttendancesByUserAndDates(this.userId, startDate, endDate)
       .subscribe((response) => {
-        this.attendances = response.attendances.map(
-          (attendance: string) => new Date(attendance)
-        );
+        console.log(response)
+        this.attendances = response.attendances.map((attendance: Attendance) => ({
+          ...attendance,
+          date: this.convertToLocalDate(new Date(attendance.date))
+        }));
       });
+  }
+
+  convertToLocalDate(date: Date): string {
+    const peruOffset = -5 * 60; 
+    const localDate = new Date(date.getTime() + peruOffset * 60000);
+    return localDate.toISOString(); 
   }
 
   getAttendanceClass(date: Date): string {
@@ -55,18 +71,24 @@ export class AttendanceMonthSummaryComponent implements OnInit {
       return ''; 
     }
     const attendance = this.attendances.find(
-      (att) => att.toDateString() === date.toDateString()
+      (att) => new Date(att.date).toDateString() === date.toDateString()
     );
 
     if (attendance) {
-      const hour = attendance.getHours();
-      if (hour < 9) {
-        return 'green';
-      } else {
-        return 'orange';
+      switch (attendance.state) {
+        case 'Asistencia':
+          return 'green';
+        case 'Tardanza':
+          return 'orange';
+        case 'Falta':
+          return 'red';
+        case 'Justificado':
+          return 'blue';
+        default:
+          return '';
       }
     } else {
-      return 'red';
+      return 'red'; 
     }
   }
 
@@ -76,35 +98,44 @@ export class AttendanceMonthSummaryComponent implements OnInit {
       return ''; 
     }
     const attendance = this.attendances.find(
-      (att) => att.toDateString() === date.toDateString()
+      (att) => new Date(att.date).toDateString() === date.toDateString()
     );
 
     if (attendance) {
-      const hour = attendance.getHours();
-      if (hour < 9) {
-        return 'A';
-      } else {
-        return 'T';
+      switch (attendance.state) {
+        case 'Asistencia':
+          return 'A';
+        case 'Tardanza':
+          return 'T';
+        case 'Falta':
+          return 'F';
+        case 'Justificado':
+          return 'J';
+        default:
+          return '';
       }
     } else {
-      return 'F';
+      return 'F'; 
     }
   }
-  generateTooltip(attendanceDate: Date ): string {
+
+  generateTooltip(attendanceDate: Date): string {
     const attendance = this.attendances.find(
-      (att) => att.toDateString() === attendanceDate.toDateString()
+      (att) => new Date(att.date).toDateString() === attendanceDate.toDateString()
     );
-    if(attendance){
-      const day = attendance.getDate();
-      const month = attendance.toLocaleString('es-ES', { month: 'long' });
-      const year = attendance.getFullYear();
-      const hours = String(attendance.getHours()).padStart(2, '0');
-      const minutes = String(attendance.getMinutes()).padStart(2, '0');
-      const seconds = String(attendance.getSeconds()).padStart(2, '0');
+
+    if (attendance) {
+      const localDate = new Date(attendance.date);
+      const day = localDate.getDate();
+      const month = localDate.toLocaleString('es-ES', { month: 'long' });
+      const year = localDate.getFullYear();
+      const hours = String(localDate.getHours()).padStart(2, '0');
+      const minutes = String(localDate.getMinutes()).padStart(2, '0');
+      const seconds = String(localDate.getSeconds()).padStart(2, '0');
 
       return `${day} de ${month} del ${year} a las ${hours}:${minutes}:${seconds}`;
-
     }
+
     return 'No tiene registrada la asistencia para este día';
   }
 }
