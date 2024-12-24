3fe82115d2cc2a08cc6c18419e3b37d9c584227e
Anderson Ore <u202011467@upc.edu.pe>
2024-12-24 17:51:22 -0500

    Update environment.ts

--- src/app/env/environment.ts
+++ src/app/env/environment.ts
@@ -1,14 +1,14 @@
-export const environment={
-    //apiUrl:'http://34.69.227.240:3000'
-    //apiUrl:'https://stargibackend-production.up.railway.app'
-    apiUrl:'http://localhost:3000',
+export const environment = {
+  //apiUrl:'http://34.69.227.240:3000'
+  apiUrl: 'https://stargibackend-production.up.railway.app',
+  //apiUrl:'http://localhost:3000',
 
-    firebaseConfig: {
-        apiKey: "AIzaSyBbk9R-9gwjPgSnmPThm77l41AFo4N5vd0",
-        authDomain: "test-stargi-storage.firebaseapp.com",
-        projectId: "test-stargi-storage",
-        storageBucket: "gs://test-stargi-storage.firebasestorage.app",
-        messagingSenderId: "280321114042",
-        appId: "1:280321114042:web:fb262fba1497a05571e783"
-      }
-}
+  firebaseConfig: {
+    apiKey: 'AIzaSyBbk9R-9gwjPgSnmPThm77l41AFo4N5vd0',
+    authDomain: 'test-stargi-storage.firebaseapp.com',
+    projectId: 'test-stargi-storage',
+    storageBucket: 'gs://test-stargi-storage.firebasestorage.app',
+    messagingSenderId: '280321114042',
+    appId: '1:280321114042:web:fb262fba1497a05571e783',
+  },
+};
